c9ee77f3d9fed02621baaf98e6d204a3978ec658
Ernesto Amaya Rocha <ernestoar2301@gmail.com>
2024-09-03 17:22:52 -0600

    ft cambios en el dashboard

--- src/components/dashboard/layout/nav-icons.tsx
+++ src/components/dashboard/layout/nav-icons.tsx
@@ -5,12 +5,23 @@ import { PlugsConnected as PlugsConnectedIcon } from '@phosphor-icons/react/dist
 import { User as UserIcon } from '@phosphor-icons/react/dist/ssr/User';
 import { Users as UsersIcon } from '@phosphor-icons/react/dist/ssr/Users';
 import { XSquare } from '@phosphor-icons/react/dist/ssr/XSquare';
+import {CashRegister} from '@phosphor-icons/react/dist/ssr/CashRegister';
+import {Cheese} from '@phosphor-icons/react/dist/ssr/Cheese';
+import {Package} from '@phosphor-icons/react/dist/ssr/Package';
+import {AddressBook} from '@phosphor-icons/react/dist/ssr/AddressBook';
+
+
 
 export const navIcons = {
   'chart-pie': ChartPieIcon,
   'gear-six': GearSixIcon,
   'plugs-connected': PlugsConnectedIcon,
   'x-square': XSquare,
+  'cash-register': CashRegister,
+  'cheese': Cheese,
+  'package': Package,
+  'address-book': AddressBook,
+  
   user: UserIcon,
   users: UsersIcon,
 } as Record<string, Icon>;
